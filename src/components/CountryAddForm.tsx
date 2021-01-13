8f008e2e5020f14fa76de0cb7252409b97d2053f
Kangdyu <kangdyu99@gmail.com>
2021-01-13 23:45:03 +0900

    [add] country add form

---
+++ src/components/CountryAddForm.tsx
@@ -0,0 +1,110 @@
+import { Fragment, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import styled from 'styled-components';
+import { ICountryInfo } from '../api/types';
+import { useAppDispatch } from '../store';
+import { createCountryItem } from '../store/countriesSlice';
+
+const Form = styled.form`
+  width: 100%;
+  padding: 20px 0;
+
+  border-radius: 10px;
+  background-color: #f1f2f3;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const FormRow = styled.div`
+  margin-bottom: 10px;
+
+  display: flex;
+  flex-direction: column;
+
+  label {
+    font-size: 0.9rem;
+    margin-bottom: 5px;
+
+    span {
+      color: red;
+    }
+  }
+
+  input {
+    font: inherit;
+  }
+`;
+
+const Error = styled.span`
+  font-size: 0.9rem;
+  color: red;
+  margin: 10px 0;
+`;
+
+function CountryAddForm() {
+  const [callingCodeCount, setCallingCodeCount] = useState(1);
+  const { register, handleSubmit, errors } = useForm<ICountryInfo>();
+  const dispatch = useAppDispatch();
+
+  const onSubmit = (data: ICountryInfo) => {
+    dispatch(createCountryItem(data));
+  };
+
+  return (
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <FormRow>
+        <label htmlFor="name">
+          나라 이름 <span>*</span>
+        </label>
+        <input id="name" name="name" ref={register({ required: true })} />
+        {errors.name && <Error>필수 입력 사항입니다.</Error>}
+      </FormRow>
+
+      <FormRow>
+        <label htmlFor="alpha2Code">
+          코드 <span>*</span>
+        </label>
+        <input
+          id="alpha2Code"
+          name="alpha2Code"
+          ref={register({ required: true })}
+        />
+        {errors.alpha2Code && <Error>필수 입력 사항입니다.</Error>}
+      </FormRow>
+
+      <FormRow>
+        <label htmlFor="region">대륙</label>
+        <input id="region" name="region" ref={register} />
+      </FormRow>
+
+      <FormRow>
+        <label htmlFor="capital">수도</label>
+        <input id="capital" name="capital" ref={register} />
+      </FormRow>
+
+      <FormRow>
+        <label htmlFor="callingCodes">국가 전화번호</label>
+        {Array.from({ length: callingCodeCount }, (_, i) => i).map((count) => (
+          <Fragment key={count}>
+            <input
+              id="callingCodes"
+              name={`callingCodes[${count}]`}
+              ref={register}
+            />
+            {count + 1 === callingCodeCount && (
+              <button onClick={() => setCallingCodeCount(callingCodeCount + 1)}>
+                +
+              </button>
+            )}
+          </Fragment>
+        ))}
+      </FormRow>
+
+      <input type="submit" />
+    </Form>
+  );
+}
+
+export default CountryAddForm;
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,12 @@
 import CountryTable from './CountryTable';
 import Banner from './Banner';
+import CountryAddForm from './CountryAddForm';
 
 function Home() {
   return (
     <>
       <Banner />
+      <CountryAddForm />
       <CountryTable />
     </>
   );
